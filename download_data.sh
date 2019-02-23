4c45c54627781a69d0fa31d38e6aaa41df943e52
Andrew Sherman <asherman@cloudera.com>
2019-02-23 13:36:51 -0800

    fix download

--- download_data.sh
+++ download_data.sh
@@ -6,13 +6,13 @@ ROLLS=Assessor-Historical-Secured-Property-Tax-Rolls.json
 if [ ! -e $ROLLS ]; then
     curl -o $ROLLS 'https://data.sfgov.org/api/views/wv5m-vpq2/rows.json?accessType=DOWNLOAD'
 else
-	echo $ROLLS exists already
+    echo $ROLLS exists already
 fi
 
 ADDR=Addresses-with-Units-Enterprise-Addressing-System.json
 if [ ! -e $ADDR ]; then
-    curl -o  'https://data.sfgov.org/api/geospatial/ramy-di5m?method=export&format=GeoJSON'
+    curl -o  $ADDR 'https://data.sfgov.org/api/geospatial/ramy-di5m?method=export&format=GeoJSON'
 else
-	echo $ADDR exists already
+    echo $ADDR exists already
 fi
 
